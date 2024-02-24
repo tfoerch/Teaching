214df7c563ecd3bf240671c0d4e8b6364bb96696
Thomas Förch <thomas@tfoerch.de>
2024-02-24 05:22:34 +0100

    add folders for other c++ standards

---
+++ c++23/ParameterPackIndexing/GetNth.cpp
@@ -0,0 +1,39 @@
+#include <utility>
+#include <iostream>
+
+#if !__cpp_pack_indexing
+template<std::size_t>
+struct dummy { constexpr dummy(auto&&){}; };
+#endif // __cpp_pack_indexing 
+
+template <std::size_t N, typename... Args>
+constexpr decltype(auto) nth_element(Args&&... args) {
+#if __cpp_pack_indexing
+    return std::forward<Args...[N](args...[N]);
+#else
+    return [&]<std::size_t... Is>(std::index_sequence<Is...>) -> decltype(auto) {
+        return [](dummy<Is> ..., auto&& arg, auto&&...) -> decltype(auto) {
+            return std::forward<decltype(arg)>(arg);
+        }(std::forward<Args>(args)...);
+    }(std::make_index_sequence<N>());
+#endif // __cpp_pack_indexing
+}
+
+int main()
+{
+#if __cpp_pack_indexing
+    std::cout << "using C++-26 pack indexing\n";
+#else
+    std::cout << "using C++-20 lambda with explicit template parameter list\n";
+#endif // __cpp_pack_indexing
+
+    static_assert(nth_element<0>(1, 2, 3) == 1);
+    static_assert(nth_element<1>(1, 2, 3) == 2);
+    static_assert(nth_element<2>(1, 2, 3) == 3);
+    
+    class A {};
+    class B {};
+    class C {};
+    static_assert(std::is_same_v<std::remove_cvref_t<decltype(nth_element<1>(A{}, B{}, C{}))>, B>);
+    return 0;
+}
